515366c79d368ece411f79628cad852a43750764
ArtsemAverkov <102361520+ArtsemAverkov@users.noreply.github.com>
2023-06-20 13:40:41 +0300

    add: news-api.yml for NewsManager and user-api.yml for UserManager

---
+++ src/test/java/ru/clevertec/settingApiManager/SettingApiManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.settingApiManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingApiManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
